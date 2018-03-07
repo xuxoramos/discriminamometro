9186d56d1f0531bb3a494c4c626708dba868a1b1
Mary Ann <MarianaGodina@box.local>
2018-03-07 17:02:19 -0600

    TF IDF

--- src/03_eda_reporte.R
+++ src/03_eda_reporte.R
@@ -8,13 +8,14 @@ library(magrittr)
 library(tidytext)
 library(tm)
 library(stringi)
+library(reshape2)
+library(wordcloud)
+library(topicmodels)
 
 base_reportes_raw <- readxl::read_xlsx("data/RPORTE HECHOS COPRED (ok).xlsx", 
                                    sheet = 1)
 
-glimpse(base_reportes)
-
-summary(base_reportes)
+glimpse(base_reportes_raw)
 
 #seleccion de variables que nos interesan
 base_reportes <- base_reportes_raw %>% 
@@ -22,7 +23,8 @@ base_reportes <- base_reportes_raw %>%
          fecha_ultima_accion, tipo_intervencion, derecho_vulnerado,
          estatus, medio_ingreso, tipo_gestion_id, lugar_hechos,
          dele_mpio, genero, ocupacion, edad_primer_contacto, grupo_pobla,
-         tipo_discri, motivo_discriminacion_id, motivo_dicriminacion) %>% 
+         tipo_discri, motivo_discriminacion_id, motivo_dicriminacion, 
+         narracion_hechos) %>% 
   mutate_if(is.character, as.factor)
 
 glimpse(base_reportes)
@@ -39,7 +41,7 @@ palabras <- comentarios %>%
 
 stop_words <- data.frame(word = c(tm::stopwords(kind = "es"), "c", "dia", 2017,
                                   "hacia", "00", "dos", "vez", "ano", "obstante",
-                                  "asi", "ser")) %>% 
+                                  "asi", "ser", "anos", "hace")) %>% 
   mutate(word = stri_trans_general(word, id = "Latin-ASCII"))
 
 tidy_words <- palabras %>%
@@ -57,3 +59,36 @@ n_pal %>%
   geom_col() +
   xlab(NULL) +
   coord_flip()
+
+#TF - IDF
+base_reportes %>% 
+  group_by(motivo_dicriminacion) %>% 
+  summarise(texto = paste0(narracion_hechos, collapse = "")) %>% 
+  mutate(tam = stri_length(texto)) %>% 
+  select(motivo_dicriminacion, tam) %>% 
+  View()
+
+base_nar_motivo <- base_reportes %>% 
+  group_by(motivo_dicriminacion) %>% 
+  summarise(texto = paste0(narracion_hechos, collapse = "")) %>% 
+  ungroup() %>% 
+  unnest_tokens(word, texto) %>% 
+  mutate(word = stri_trans_general(word, id = "Latin-ASCII")) %>% 
+  anti_join(stop_words) %>% 
+  count(word, motivo_dicriminacion) %>% 
+  na.omit() 
+
+base_nar_motivo %>% 
+  bind_tf_idf(word, motivo_dicriminacion, n) %>% 
+  acast(word ~ motivo_dicriminacion,
+        value.var = 'tf_idf',
+        fill = 0) %>% 
+  comparison.cloud(max.words = 300, scale = c(4, 0.1), title.size = 1.5)
+
+#topic modeling
+
+lda_base <- base_nar_motivo %>%
+  cast_dtm(motivo_dicriminacion, word, n) %>% 
+  topicmodels::LDA(base_nar_motivo, k = 5, control = list(seed = 1234))
+
+
