03d14d56fe18461208f8da43714f9dfc184bd7c9
marianaga89 <marianagodina@gmail.com>
2018-04-11 18:55:19 -0500

    analisis dummy tfidf

---
+++ src/04_lectura_tweets.R
@@ -0,0 +1,551 @@
+#script lectura de tweets con geocode
+# Fetch TWs
+library(twitteR)
+
+# llaves
+tw_api_key <- 'QOe4KhUucn8hRP9pJ2q2QYksn'
+tw_api_secret <- 'HYiwanxtHDuaew2mnEXgCVhVxD3zEEZo77K8ihJDZhqPBrmhdo'
+access_token <- '777720164011773953-10EFKORqQpsdFsCnfsTY4P7iko6SkIW'
+access_secret <- 'c8xKppCWwpU2veAuTW6bGMfSoSlkA1GnlD9Rh3isDbyu9'
+
+setup_twitter_oauth(tw_api_key, tw_api_secret, access_token, access_secret)
+
+
+# Género ------------------------------------------------------------------
+
+## Vieja loca
+
+tw <- twitteR::searchTwitter('vieja+loca', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+vieja_loca <- twitteR::twListToDF(tw)
+
+saveRDS(vieja_loca, "discriminating-words/data/tweets/genero/vieja_loca_geo.RDS")
+
+##mamá luchona
+
+tw <- twitteR::searchTwitter('mama+luchona', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+mama_luchona<- twitteR::twListToDF(tw)
+
+saveRDS(mama_luchona, "discriminating-words/data/tweets/genero/mama_luchona.RDS")
+
+#como niña
+
+tw <- twitteR::searchTwitter('como niña', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+como_nina <- twitteR::twListToDF(tw)
+
+saveRDS(como_nina, "discriminating-words/data/tweets/genero/como_nina.RDS")
+
+##es de niña
+
+tw <- twitteR::searchTwitter('es de niña', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+es_de_nina <- twitteR::twListToDF(tw)
+
+saveRDS(es_de_nina, "discriminating-words/data/tweets/genero/es_de_nina.RDS")
+
+#como los hombres
+
+tw <- twitteR::searchTwitter('como los hombres', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+como_hombre <- twitteR::twListToDF(tw)
+
+saveRDS(como_hombre, "discriminating-words/data/tweets/genero/como_hombre.RDS")
+
+#para qué tienes hijos
+
+tw <- twitteR::searchTwitter('para+que+tienes+hijos', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+tienes_hijos <- twitteR::twListToDF(tw)
+
+saveRDS(tienes_hijos, "discriminating-words/data/tweets/genero/tienes_hijos.RDS")
+
+#veja pendeja
+
+tw <- twitteR::searchTwitter('vieja pendeja', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+veja_pendeja <- twitteR::twListToDF(tw)
+
+saveRDS(veja_pendeja, "discriminating-words/data/tweets/genero/veja_pendeja.RDS")
+
+#discriminacion embarazada
+
+tw <- twitteR::searchTwitter('discriminacion embarazada', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+discriminacion_embarazada <- twitteR::twListToDF(tw)
+
+saveRDS(discriminacion_embarazada, "discriminating-words/data/tweets/genero/discriminacion_embarazada.RDS")
+
+#discriminacion mujeres
+
+tw <- twitteR::searchTwitter('discriminacion mujeres', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+discriminacion_mujeres <- twitteR::twListToDF(tw)
+
+saveRDS(discriminacion_mujeres, "discriminating-words/data/tweets/genero/discriminacion_mujeres.RDS")
+
+## feminazi
+
+tw <- twitteR::searchTwitter('feminazi', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+feminazi <- twitteR::twListToDF(tw)
+
+saveRDS(feminazi, "discriminating-words/data/tweets/genero/feminazi.RDS")
+
+# orientación/identidad sexual --------------------------------------------
+
+## pinche lesbiana
+
+tw <- twitteR::searchTwitter('pinche lesbiana', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_lesbiana <- twitteR::twListToDF(tw)
+
+saveRDS(pinche_lesbiana, "discriminating-words/data/tweets/orientacion/pinche_lesbiana.RDS")
+
+## lesbiana
+
+tw <- twitteR::searchTwitter('lesbiana', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+lesbiana <- twitteR::twListToDF(tw)
+
+saveRDS(lesbiana, "discriminating-words/data/tweets/orientacion/lesbiana.RDS")
+
+##Le gusta el arroz con popote
+
+tw <- twitteR::searchTwitter('arroz con popote', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+arroz_popote <- twitteR::twListToDF(tw)
+
+saveRDS(arroz_popote, "discriminating-words/data/tweets/orientacion/arroz_popote.RDS")
+
+##puñal
+
+tw <- twitteR::searchTwitter('puñal', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+punial <- twitteR::twListToDF(tw)
+
+saveRDS(punial, "discriminating-words/data/tweets/orientacion/punial.RDS")
+
+##machorra
+
+tw <- twitteR::searchTwitter('machorra', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+machorra <- twitteR::twListToDF(tw)
+
+saveRDS(machorra, "discriminating-words/data/tweets/orientacion/machorra.RDS")
+
+## Pinche maricon
+
+tw <- twitteR::searchTwitter('Pinche maricon', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_maricon <- twitteR::twListToDF(tw)
+
+saveRDS(pinche_maricon, "discriminating-words/data/tweets/orientacion/pinche_maricon.RDS")
+
+##pinche puto
+
+tw <- twitteR::searchTwitter('pinche puto', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_puto <- twitteR::twListToDF(tw)
+
+saveRDS(pinche_puto, "discriminating-words/data/tweets/orientacion/pinche_puto.RDS")
+
+##joto
+
+tw <- twitteR::searchTwitter('joto', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+joto <- twitteR::twListToDF(tw)
+
+saveRDS(joto, "discriminating-words/data/tweets/orientacion/joto.RDS")
+
+##lencha 
+
+tw <- twitteR::searchTwitter('lencha', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+lencha <- twitteR::twListToDF(tw)
+
+saveRDS(lencha, "discriminating-words/data/tweets/orientacion/lencha.RDS")
+
+
+
+
+
+
+
+
+
+
+# Ideología política ------------------------------------------------------
+
+##chairo
+
+tw <- twitteR::searchTwitter('chairo', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+chairo <- twitteR::twListToDF(tw)
+
+saveRDS(chairo, "discriminating-words/data/tweets/ideologia_politica/chairo.RDS")
+
+
+##derechairo
+
+tw <- twitteR::searchTwitter('derechairo', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+derechairo <- twitteR::twListToDF(tw)
+
+saveRDS(derechairo, "discriminating-words/data/tweets/ideologia_politica/derechairo.RDS")
+
+##feminazi
+
+tw <- twitteR::searchTwitter('feminazi', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+feminazi <- twitteR::twListToDF(tw)
+
+saveRDS(feminazi, "discriminating-words/data/tweets/ideologia_politica/feminazi.RDS")
+
+##
+# Apariencia (tatuajes, pobreza, condición económica o social) ------------
+
+## Naco
+
+tw <- twitteR::searchTwitter('naco', 
+                             n = 5000, 
+                             retryOnRateLimit = 500,
+                             geocode='19.382099,-99.136102,90km')
+
+naco <- twitteR::twListToDF(tw)
+
+saveRDS(naco, "discriminating-words/data/tweets/apariencia/naco.RDS")
+
+## jodido
+
+tw <- twitteR::searchTwitter('jodido', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+jodido <- twitteR::twListToDF(tw)
+
+saveRDS(jodido, "discriminating-words/data/tweets/apariencia/jodido.RDS")
+
+## iztapalacra
+
+tw <- twitteR::searchTwitter('iztapalacra', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+iztapalacra <- twitteR::twListToDF(tw)
+
+saveRDS(iztapalacra, "discriminating-words/data/tweets/apariencia/iztapalacra.RDS")
+
+## chacha
+
+tw <- twitteR::searchTwitter('chacha', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+chacha <- twitteR::twListToDF(tw)
+
+saveRDS(chacha, "discriminating-words/data/tweets/apariencia/chacha.RDS")
+
+## pinche_fresa
+
+tw <- twitteR::searchTwitter('pinche fresa', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_fresa <- twitteR::twListToDF(tw)
+
+saveRDS(pinche_fresa, "discriminating-words/data/tweets/apariencia/pinche_fresa.RDS")
+
+## pinche negro
+
+tw <- twitteR::searchTwitter('pinche negro', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_negro <- twitteR::twListToDF(tw)
+
+saveRDS(pinche_negro, "discriminating-words/data/tweets/apariencia/pinche_negro.RDS")
+
+## pinche gordo
+
+tw <- twitteR::searchTwitter('pinche gordo', 
+                             n = 5000, 
+                             retryOnRateLimit = 500,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_gordo<- twitteR::twListToDF(tw)
+
+saveRDS(pinche_gordo, "discriminating-words/data/tweets/apariencia/pinche_gordo.RDS")
+
+## pinche indio
+
+tw <- twitteR::searchTwitter('pinche indio', 
+                             n = 5000, 
+                             retryOnRateLimit = 500,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_indio<- twitteR::twListToDF(tw)
+
+saveRDS(indio, "discriminating-words/data/tweets/apariencia/pinche_indio.RDS")
+
+## pinche pobre
+
+tw <- twitteR::searchTwitter('pinche pobre', 
+                             n = 5000, 
+                             retryOnRateLimit = 500,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_pobre<- twitteR::twListToDF(tw)
+
+saveRDS(pinche_pobre, "discriminating-words/data/tweets/apariencia/pinche_pobre.RDS")
+
+## pinche indio
+
+tw <- twitteR::searchTwitter('pinche indio', 
+                             n = 5000, 
+                             retryOnRateLimit = 500,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_indio<- twitteR::twListToDF(tw)
+
+saveRDS(pinche_indio, "discriminating-words/data/tweets/apariencia/pinche_indio.RDS")
+
+## guerito
+
+tw <- twitteR::searchTwitter('guerito', 
+                             n = 5000, 
+                             retryOnRateLimit = 500,
+                             geocode='19.382099,-99.136102,90km')
+
+guerito <- twitteR::twListToDF(tw)
+
+saveRDS(guerito, "discriminating-words/data/tweets/apariencia/guerito.RDS")
+
+# religion ----------------------------------------------------------------
+
+## pinche judio
+
+tw <- twitteR::searchTwitter('pinche judio', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_judio <- twitteR::twListToDF(tw)
+
+saveRDS(pinche_judio, "discriminating-words/data/tweets/religion/pinche_judio_geo.RDS")
+
+## testiculos de jehova
+tw <- twitteR::searchTwitter('testiculos de jehova', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+testiculos_jehova <- twitteR::twListToDF(tw)
+
+saveRDS(testiculos_jehova, "discriminating-words/data/tweets/religion/testiculos_jehova.RDS")
+
+##testigos de Jehova
+
+tw <- twitteR::searchTwitter('testigos de Jehova', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+testigos_jehova <- twitteR::twListToDF(tw)
+
+saveRDS(testigos_jehova, "discriminating-words/data/tweets/religion/testigos_jehova.RDS")
+
+##testigos de Jehova
+
+tw <- twitteR::searchTwitter('religioso pendejo', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+religioso_pendejo <- twitteR::twListToDF(tw)
+
+saveRDS(religioso_pendejo, "discriminating-words/data/tweets/religion/religioso_pendejo.RDS")
+
+
+
+# edad --------------------------------------------------------------------
+
+## pinche viejo
+
+tw <- twitteR::searchTwitter('pinche viejo', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_viejo<- twitteR::twListToDF(tw)
+
+saveRDS(pinche_viejo, "discriminating-words/data/tweets/edad/pinche_vieji.RDS")
+
+## pinche ninio
+
+tw <- twitteR::searchTwitter('pinche nino', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pinche_ninio<- twitteR::twListToDF(tw)
+
+saveRDS(pinche_ninio, "discriminating-words/data/tweets/edad/pinche_ninio.RDS")
+
+## pareces_nino_chiquito
+
+tw <- twitteR::searchTwitter('pareces nino chiquito', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+pareces_nino_chiquito<- twitteR::twListToDF(tw)
+
+saveRDS(pareces_nino_chiquito, "discriminating-words/data/tweets/edad/pareces_nino_chiquito.RDS")
+
+##es_chavo
+
+tw <- twitteR::searchTwitter('es chavo', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+es_chavo <- twitteR::twListToDF(tw)
+
+saveRDS(es_chavo, "discriminating-words/data/tweets/edad/es_chavo.RDS")
+
+##chavoruco 
+
+tw <- twitteR::searchTwitter('chavorruco', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+tw_2 <- twitteR::searchTwitter('chavoruco', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+chavoruco <- rbind(twitteR::twListToDF(tw), twitteR::twListToDF(tw_2))
+
+saveRDS(chavoruco, "discriminating-words/data/tweets/edad/chavoruco.RDS")
+
+##nini
+
+tw <- twitteR::searchTwitter('NINI', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+nini <- twitteR::twListToDF(tw)
+
+saveRDS(nini, "discriminating-words/data/tweets/edad/nini.RDS")
+
+
+# discapacidad ------------------------------------------------------------
+
+##ciego
+tw <- twitteR::searchTwitter('ciego', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+ciego <- twitteR::twListToDF(tw)
+
+saveRDS(ciego, "discriminating-words/data/tweets/discapacidad/ciego.RDS")
+
+##cojo
+tw <- twitteR::searchTwitter('cojo', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+cojo <- twitteR::twListToDF(tw)
+
+saveRDS(cojo, "discriminating-words/data/tweets/discapacidad/cojo.RDS")
+
+##discapacitado
+
+tw <- twitteR::searchTwitter('discapacitado', 
+                             n = 10000, 
+                             retryOnRateLimit = 1000,
+                             geocode='19.382099,-99.136102,90km')
+
+discapacitado <- twitteR::twListToDF(tw)
+
+saveRDS(discapacitado, "discriminating-words/data/tweets/discapacidad/discapacitado.RDS")
+
+
+
---
+++ src/05_junatndo_bases.R
@@ -0,0 +1,203 @@
+#Script para juntar datos
+
+library(tidyverse)
+
+
+# género ------------------------------------------------------------------
+
+como_nina <- read_rds("discriminating-words/data/tweets/genero/como_nina.RDS") %>% 
+  mutate(tipo = "como_nina")
+
+es_de_nina <- read_rds("discriminating-words/data/tweets/genero/es_de_nina.RDS") %>% 
+  mutate(tipo = "es_de_nina")
+
+mama_luchona <- read_rds("discriminating-words/data/tweets/genero/mama_luchona.RDS") %>% 
+  mutate(tipo = "mama_luchona")
+
+vieja_loca <- read_rds("discriminating-words/data/tweets/genero/vieja_loca_geo.RDS") %>% 
+  mutate(tipo = "vieja_loca")
+
+como_hombre <- read_rds("discriminating-words/data/tweets/genero/como_hombre.RDS") %>% 
+  mutate(tipo = "como_hombre")
+
+tienes_hijos <- read_rds("discriminating-words/data/tweets/genero/tienes_hijos.RDS") %>% 
+  mutate(tipo = "tienes_hijos")
+
+vieja_pendeja <- read_rds("discriminating-words/data/tweets/genero/veja_pendeja.RDS") %>% 
+  mutate(tipo = "vieja_pendeja")
+
+discriminacion_embarazada <- read_rds("discriminating-words/data/tweets/genero/discriminacion_embarazada.RDS") %>% 
+  mutate(tipo = "discriminacion_embarazada")
+
+discriminacion_mujeres <- read_rds("discriminating-words/data/tweets/genero/discriminacion_mujeres.RDS") %>% 
+  mutate(tipo = "discriminacion_mujeres")
+
+feminazi <- read_rds("discriminating-words/data/tweets/genero/feminazi.RDS") %>% 
+  mutate(tipo = "feminazi")
+
+base_genero <- rbind(como_nina, es_de_nina, mama_luchona, vieja_loca, como_hombre,
+                     tienes_hijos, vieja_pendeja, discriminacion_embarazada, 
+                     discriminacion_mujeres, feminazi) %>% 
+  mutate(motivo = "genero")
+
+saveRDS(base_genero, "discriminating-words/data/tweets/base_genero.RDS")
+
+
+# orientación/identidad sexual --------------------------------------------
+
+arroz_popote <- read_rds("discriminating-words/data/tweets/orientacion/arroz_popote.RDS") %>% 
+  mutate(tipo = "arroz_popote")
+
+joto <- read_rds("discriminating-words/data/tweets/orientacion/joto.RDS") %>% 
+  mutate(tipo = "joto")
+
+lencha <- read_rds("discriminating-words/data/tweets/orientacion/lencha.RDS") %>% 
+  mutate(tipo = "lencha")
+
+lesbiana <- read_rds("discriminating-words/data/tweets/orientacion/lesbiana.RDS") %>% 
+  mutate(tipo = "lesbiana")
+
+pinche_lesbiana <- read_rds("discriminating-words/data/tweets/orientacion/pinche_lesbiana.RDS") %>% 
+  mutate(tipo = "pinche_lesbiana")
+
+pinche_maricon <- read_rds("discriminating-words/data/tweets/orientacion/pinche_maricon.RDS") %>% 
+  mutate(tipo = "pinche_maricon")
+
+pinche_puto <- read_rds("discriminating-words/data/tweets/orientacion/pinche_puto.RDS") %>% 
+  mutate(tipo = "pinche_puto")
+
+punial <- read_rds("discriminating-words/data/tweets/orientacion/punial.RDS") %>% 
+  mutate(tipo = "punial")
+
+base_orientacion <- rbind(arroz_popote, joto, lencha, lesbiana, pinche_lesbiana,
+                          pinche_maricon, pinche_puto, punial) %>% 
+  mutate(motivo = "orientacion")
+
+
+saveRDS(base_orientacion, "discriminating-words/data/tweets/base_orientacion.RDS")
+
+
+# Ideología política ------------------------------------------------------
+
+chairo <- read_rds("discriminating-words/data/tweets/ideologia_politica/chairo.RDS") %>% 
+  mutate(tipo = "chairo")
+
+derechairo <- read_rds("discriminating-words/data/tweets/ideologia_politica/derechairo.RDS") %>% 
+  mutate(tipo = "derechairo")
+
+feminazi <- read_rds("discriminating-words/data/tweets/ideologia_politica/feminazi.RDS") %>% 
+  mutate(tipo = "feminazi")
+
+base_ideologia <- rbind(chairo, derechairo, feminazi) %>% 
+  mutate(motivo = "ideoligia")
+
+
+saveRDS(base_ideologia, "discriminating-words/data/tweets/base_ideologia.RDS")
+
+
+# apariencia --------------------------------------------------------------
+
+naco <- read_rds("discriminating-words/data/tweets/apariencia/naco.RDS") %>% 
+  mutate(tipo = "naco")
+
+jodido <- read_rds("discriminating-words/data/tweets/apariencia/jodido.RDS") %>% 
+  mutate(tipo = "jodido")
+
+iztapalacra <- read_rds("discriminating-words/data/tweets/apariencia/iztapalacra.RDS") %>% 
+  mutate(tipo = "iztapalacra")
+
+chacha <- read_rds("discriminating-words/data/tweets/apariencia/chacha.RDS") %>% 
+  mutate(tipo = "chacha")
+
+pinche_fresa <- read_rds("discriminating-words/data/tweets/apariencia/pinche_fresa.RDS") %>% 
+  mutate(tipo = "pinche_fresa")
+
+pinche_negro <- read_rds("discriminating-words/data/tweets/apariencia/pinche_negro.RDS") %>% 
+  mutate(tipo = "pinche_negro")
+
+pinche_gordo <- read_rds("discriminating-words/data/tweets/apariencia/pinche_gordo.RDS") %>% 
+  mutate(tipo = "pinche_gordo")
+
+pinche_indio <- read_rds("discriminating-words/data/tweets/apariencia/pinche_indio.RDS") %>% 
+  mutate(tipo = "pinche_indio")
+
+pinche_pobre <- read_rds("discriminating-words/data/tweets/apariencia/pinche_pobre.RDS") %>% 
+  mutate(tipo = "pinche_pobre")
+
+guerito <- read_rds("discriminating-words/data/tweets/apariencia/guerito.RDS") %>% 
+  mutate(tipo = "guerito")
+
+base_apariencia <- rbind(naco, jodido, iztapalacra, chacha, pinche_fresa,
+                         pinche_negro, pinche_gordo, pinche_indio, pinche_pobre,
+                         guerito) %>% 
+  mutate(motivo = "ideoligia")
+
+
+saveRDS(base_apariencia, "discriminating-words/data/tweets/base_apariencia.RDS")
+
+# religion ----------------------------------------------------------------
+
+pinche_judio <- read_rds("discriminating-words/data/tweets/religion/pinche_judio_geo.RDS") %>% 
+  mutate(tipo = "pinche_judio")
+
+testiculos_jehova <- read_rds("discriminating-words/data/tweets/religion/testiculos_jehova.RDS") %>% 
+  mutate(tipo = "testiculos_jehova")
+
+testigos_jehova <- read_rds("discriminating-words/data/tweets/religion/testigos_jehova.RDS") %>% 
+  mutate(tipo = "testigos_jehova")
+
+religioso_pendejo <- read_rds("discriminating-words/data/tweets/religion/religioso_pendejo.RDS") %>% 
+  mutate(tipo = "religioso_pendejo")
+
+base_religion <- rbind(pinche_judio, testiculos_jehova, testigos_jehova, religioso_pendejo) %>% 
+  mutate(motivo = "religion")
+
+saveRDS(base_religion, "discriminating-words/data/tweets/base_religion.RDS")
+
+
+
+
+
+# edad --------------------------------------------------------------------
+
+pinche_viejo <- read_rds("discriminating-words/data/tweets/edad/pinche_vieji.RDS") %>% 
+  mutate(tipo = "pinche_viejo")
+
+chavoruco <- read_rds("discriminating-words/data/tweets/edad/chavoruco.RDS") %>% 
+  mutate(tipo = "chavoruco")
+
+es_chavo <- read_rds("discriminating-words/data/tweets/edad/es_chavo.RDS") %>% 
+  mutate(tipo = "es_chavo")
+
+nini <- read_rds("discriminating-words/data/tweets/edad/nini.RDS") %>% 
+  mutate(tipo = "nini")
+
+pareces_nino_chiquito <- read_rds("discriminating-words/data/tweets/edad/pareces_nino_chiquito.RDS") %>% 
+  mutate(tipo = "pareces_nino_chiquito")
+
+pinche_ninio <- read_rds("discriminating-words/data/tweets/edad/pinche_ninio.RDS") %>% 
+  mutate(tipo = "pinche_ninio")
+
+base_edad <- rbind(pinche_viejo, chavoruco, es_chavo, nini, pareces_nino_chiquito,pinche_ninio) %>% 
+  mutate(motivo = "edad")
+
+saveRDS(base_edad, "discriminating-words/data/tweets/base_edad.RDS")
+
+
+
+# discapacidad ------------------------------------------------------------
+ciego <- read_rds("discriminating-words/data/tweets/discapacidad/ciego.RDS") %>% 
+  mutate(tipo = "ciego")
+
+cojo <- read_rds("discriminating-words/data/tweets/discapacidad/cojo.RDS") %>% 
+  mutate(tipo = "cojo")
+
+discapacitado <- read_rds("discriminating-words/data/tweets/discapacidad/discapacitado.RDS") %>% 
+  mutate(tipo = "discapacitado")
+
+base_discapacidad <- rbind(ciego, cojo, discapacitado) %>% 
+  mutate(motivo = "discapacidad")
+
+saveRDS(base_discapacidad, "discriminating-words/data/tweets/base_discapacidad.RDS")
+
+
