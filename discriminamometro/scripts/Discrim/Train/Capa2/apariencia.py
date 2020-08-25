14c6750f70ae29c48c48b42d4bd6e393f51af8dc
ronmoy007 <ronmoy007@hotmail.com>
2020-08-25 05:57:04 +0000

    Se ajusta seccion Train

--- discriminamometro/scripts/Discrim/Train/Capa2/apariencia.py
+++ discriminamometro/scripts/Discrim/Train/Capa2/apariencia.py
@@ -1,22 +1,26 @@
 import pandas as pd
 import Discrim.utileria as ut
-import Discrim.magic_loop as mgl
+from Discrim.Train import base_train
 import os
 import spacy
 import nltk
 import pickle as pickle
 from sklearn.model_selection import train_test_split
 import numpy as np
+import dask.dataframe as dd
 
-class Apariencia():
+class Apariencia(base_train.BaseTrain):
     
     def __init__(self):
         
-        self.obj_utileria = ut.Utileria()
+        # Instanciamos la clase padre
+        super().__init__()
+        
+        # self.obj_utileria = ut.Utileria()
         self.str_ruta_entrenamiento = '00_entrenamiento/tweets_modelo_capa2'
-        self.str_fuente_clasificacion = 'tweets_entrenamiento_genero_light.csv'
+        self.str_fuente_clasificacion = 'tweets_entrenamiento_apariencia_balance.csv'
         self.str_LocalFile = 'data/tweets/' + self.str_fuente_clasificacion
-        self.str_nombrePickle = 'modelo_capa2_apariencia.p'
+        self.str_nombrePickle = 'modelo_capa2_apariencia_prueba.p'
         self.str_ruta_s3_modelos = '01_modelos/capa2/apariencia'
         self.str_modelo_pickle_s3 = self.str_ruta_s3_modelos+'/'+self.str_nombrePickle
         
@@ -42,12 +46,12 @@ class Apariencia():
         self.npDictHiperParam = np.append(self.npDictHiperParam, dictHyperParams)
 
         # Parametrización para XGBoost
-        dictHyperParams = {'learning_rate': [0.1,0.25, 0.75],
-                           'n_estimators': [50,100,150],  # Se redujo a 50
+        dictHyperParams = {'learning_rate': [0.1,0.75], #0.25
+                           'n_estimators': [100],  # Se redujo a 50
                            'min_samples_split': [4,16],
-                           'min_samples_leaf': [3,7],
-                           'max_depth': [3,4,5,6,7,10,15],
-                           'max_features': ['sqrt','log2']
+                           'min_samples_leaf': [3],
+                   'max_depth': [3,6,7],
+                           'max_features': ['sqrt'] #listo amigo
                            }
         self.npDictHiperParam = np.append(self.npDictHiperParam, dictHyperParams)
 
@@ -60,100 +64,4 @@ class Apariencia():
         self.nbv_cross_validation = 5
         self.str_metric = 'f1'
         
-        return
-
-    def cargar_modelo_embeddings(self):
-        
-        self.nlp = spacy.load('./data/')
-        
-        return
-    
-    def descargar_tweets_entrenamiento(self):
-        
-        s3 = self.obj_utileria.crear_conexion_s3()
-        
-        with open(self.str_LocalFile, 'wb') as f:
-            s3.download_fileobj(self.obj_utileria.str_NombreBucket,  self.str_ruta_entrenamiento+'/'+self.str_fuente_clasificacion, f)
-        
-        return
-    
-    def transformar_dataset(self):
-        
-        # self.pd_fuente = pd.read_csv('tmp_fuente_embeddings.csv', lineterminator='\n') 
-        self.pd_fuente = pd.read_csv(self.str_LocalFile, lineterminator='\n') 
-        
-        self.obj_utileria.cargar_stop_words()
-        self.obj_utileria.descargar_punkt()
-                
-        self.pd_fuente = self.obj_utileria.formatear_dataframe(self.pd_fuente)
-        
-        
-        return
-    
-    def train(self):
-        
-        self.train, self.test = train_test_split(self.pd_fuente, test_size=0.2,random_state = 202008)
-        
-        self.cargar_modelo_embeddings()
-        npEmbeddings = self.generar_embeddings(self.train)
-        
-        obj_mgl = mgl.MagicLoop()
-        
-        # obj_mgl.train(npEmbeddings, self.train.label, )
-        
-        X_train = pd.DataFrame(npEmbeddings)
-        Y_train = pd.DataFrame(self.train.label)
-        arrModelos = obj_mgl.prep_modelos(self.npNombreModelos)
-
-        # #Se corre el magic loop para realizar las predicciones con los parámetros previamente establecidos
-        self.best_model, npGridSearchCv = obj_mgl.correr_magic_loop(arrModelos,
-                                            self.npDictHiperParam,
-                                            X_train,
-                                            Y_train,
-                                            self.nbv_cross_validation,
-                                            self.str_metric)    
-    
-    def generar_embeddings(self, fuente):
-        
-        npEmbeddings = np.empty([0, 300])
-
-        for texto in fuente['full_text']:
-
-            #print(texto)
-
-            # process a sentence using the model
-            doc = self.nlp(texto)
-
-            # print(doc.vector.shape)
-            if doc.vector.shape[0]==300:
-                npEmbeddings = np.append(npEmbeddings, [doc.vector], axis = 0)
-            else:
-                npAux = np.empty([1, 300])
-                npEmbeddings = np.append(npEmbeddings, npAux, axis = 0)
-        
-        fuente['embeddings'] = list(npEmbeddings)
-        return fuente 
-    
-    def crear_pickle(self):
-    
-        pickleFile = open(self.str_nombrePickle, 'wb')
-        pickle.dump(self.best_model, pickleFile)
-        pickleFile.close()
-        
-        return
-        
-    def mandar_pickle_s3(self):
-
-        # Una vez generado el archivo, lo envíamos a S3
-        
-        cnx_S3 = self.obj_utileria.crear_conexion_s3()
-
-        # Mandamos el archivo descargado a S3
-        try:
-            self.obj_utileria.mandar_archivo_s3(cnx_S3, self.obj_utileria.str_NombreBucket, self.str_ruta_s3_modelos+'/', self.str_nombrePickle)
-        except Exception as e:
-            with open('Error_mandar_pickle_s3.txt', 'w') as file:
-                file.write(str(e))
-            raise
-
         return
