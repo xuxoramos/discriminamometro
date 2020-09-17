ec4702e31aa38e05b562a461d270a326be0e42a0
Francisco <discriminamometro@ip-172-31-20-15.us-east-2.compute.internal>
2020-09-17 01:25:47 +0000

    Ajustes para la puesta en produccion

--- discriminamometro/scripts/Discrim/cron_descargas.sh
+++
@@ -1,5 +0,0 @@
-cd "$(dirname "$0")";
-ls > validador_cronprueba.txt
-export PATH=/home/jupyter-ronmoy007@gmail.co-0e856/.local/bin:/opt/tljh/user/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
-python3 /home/jupyter-ronmoy007@gmail.co-0e856/discriminamometro/discriminamometro/scripts/script_descargas.py
-
